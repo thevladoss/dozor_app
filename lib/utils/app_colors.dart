a9f6ece0a48a071cd43e0df6a72550fa841f9fef
Vladislav Osin <osinvladislav@yandex.ru>
2023-06-29 17:58:09 +0300

    Main page ready

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const primary = Color(0xff1657A1);
   static const primaryLight = Color(0xff5F7BD0);
+  static const primaryDark = Color(0xff2E62AE);
 }
